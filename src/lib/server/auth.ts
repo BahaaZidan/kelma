6a13cdabce1b760c359669e320f57c4491461f29
BahaaZidan <gebnatorky@gmail.com>
2025-05-18 12:25:36 +0300

    better auth (github) + bunch more

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+
+import { db } from './db';
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: 'sqlite',
+	}),
+	socialProviders: {
+		github: {
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET,
+		},
+	},
+});
