1b28bfccc867da45ae62569ef2374f0e62ae141f
BahaaZidan <gebnatorky@gmail.com>
2025-05-22 10:20:34 +0300

    fix better-auth

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,10 +3,30 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 import { DATABASE_URL } from '$env/static/private';
 
-import * as schema from './schema';
+import {
+	accountTable,
+	commentTable,
+	pageTable,
+	sessionTable,
+	userTable,
+	verificationTable,
+	websiteTable,
+} from './schema';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = new Database(DATABASE_URL);
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, {
+	schema: {
+		// DON'T CHANGE - START (better-auth expects these names)
+		user: userTable,
+		session: sessionTable,
+		account: accountTable,
+		verification: verificationTable,
+		// DON'T CHANGE - END
+		website: websiteTable,
+		page: pageTable,
+		comment: commentTable,
+	},
+});
