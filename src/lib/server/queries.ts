ccd03887e5c6c1713a0503f15d96f597f93154bd
BahaaZidan <gebnatorky@gmail.com>
2025-06-14 09:09:17 +0300

    make cursorPaginatedComments shared

--- src/lib/server/queries.ts
+++ src/lib/server/queries.ts
@@ -1,4 +1,6 @@
-import { desc, eq } from 'drizzle-orm';
+import { error, json } from '@sveltejs/kit';
+import { and, desc, eq, lt, SQL } from 'drizzle-orm';
+import * as v from 'valibot';
 
 import { db } from './db';
 import { commentTable, userTable } from './db/schema';
@@ -28,3 +30,47 @@ export const commentBaseQuery = db
 
 type CommentsBaseQueryResult = Awaited<typeof commentBaseQuery>;
 export type CursorPaginatedComments = CursorPaginated<CommentsBaseQueryResult, 'comments'>;
+
+const searchParamsSchema = v.object({
+	cursor: v.optional(
+		v.pipe(
+			v.string(),
+			v.transform((i) => Number(i)),
+			v.number(),
+			v.safeInteger(),
+			v.minValue(1)
+		)
+	),
+	pageSize: v.optional(
+		v.pipe(
+			v.string(),
+			v.transform((i) => Number(i)),
+			v.number(),
+			v.safeInteger(),
+			v.minValue(5),
+			v.maxValue(30)
+		)
+	),
+});
+
+export async function fetchCursorPaginatedComments(url: URL, where: SQL) {
+	const searchParamsValidation = v.safeParse(
+		searchParamsSchema,
+		Object.fromEntries(url.searchParams.entries())
+	);
+
+	if (!searchParamsValidation.success)
+		return error(400, searchParamsValidation.issues.map((i) => i.message).join('\n'));
+	const searchParams = searchParamsValidation.output;
+
+	const cursor = searchParams.cursor;
+	const pageSize = searchParams.pageSize || 10;
+
+	const commentsWExtraOne = await commentBaseQuery
+		.where(and(where, cursor ? lt(commentTable.id, cursor) : undefined))
+		.limit(pageSize + 1);
+
+	const comments = commentsWExtraOne.slice(0, pageSize);
+
+	return json({ comments, hasNextPage: commentsWExtraOne.length > comments.length });
+}
--- src/routes/api/[website_id]/comments/+server.ts
+++ src/routes/api/[website_id]/comments/+server.ts
@@ -1,57 +1,15 @@
-import { error, json } from '@sveltejs/kit';
-import { and, eq, lt } from 'drizzle-orm';
-import * as v from 'valibot';
+import { error } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
 
 import { commentTable } from '$lib/server/db/schema';
-import { commentBaseQuery } from '$lib/server/queries';
+import { fetchCursorPaginatedComments } from '$lib/server/queries';
 
 import type { RequestHandler } from './$types';
-
-const searchParamsSchema = v.object({
-	cursor: v.optional(
-		v.pipe(
-			v.string(),
-			v.transform((i) => Number(i)),
-			v.number(),
-			v.safeInteger(),
-			v.minValue(1)
-		)
-	),
-	pageSize: v.optional(
-		v.pipe(
-			v.string(),
-			v.transform((i) => Number(i)),
-			v.number(),
-			v.safeInteger(),
-			v.minValue(5),
-			v.maxValue(30)
-		)
-	),
-});
 
 export const GET: RequestHandler = async ({ params, locals, url }) => {
 	const websiteId = Number(params.website_id);
 	if (!locals.session?.websitesOwnedByCurrentUser?.includes(websiteId)) return error(400);
 
-	const searchParamsValidation = v.safeParse(
-		searchParamsSchema,
-		Object.fromEntries(url.searchParams.entries())
-	);
-
-	if (!searchParamsValidation.success)
-		return error(400, searchParamsValidation.issues.map((i) => i.message).join('\n'));
-	const searchParams = searchParamsValidation.output;
-
-	const cursor = searchParams.cursor;
-	const pageSize = searchParams.pageSize || 10;
-
-	const commentsWExtraOne = await commentBaseQuery
-		.where(
-			and(eq(commentTable.websiteId, websiteId), cursor ? lt(commentTable.id, cursor) : undefined)
-		)
-		.limit(pageSize + 1);
-
-	const comments = commentsWExtraOne.slice(0, pageSize);
-
-	return json({ comments, hasNextPage: commentsWExtraOne.length > comments.length });
+	const response = await fetchCursorPaginatedComments(url, eq(commentTable.websiteId, websiteId));
+	return response;
 };
