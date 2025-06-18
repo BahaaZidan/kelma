5a2a88b1545ffc68f1253b1ba1e662cd6694e913
BahaaZidan <gebnatorky@gmail.com>
2025-06-18 13:09:04 +0300

    use isomorphic js-base64 instead of Node's built-in Buffer

--- src/lib/global-id-utils.ts
+++ src/lib/global-id-utils.ts
@@ -1,8 +1,10 @@
+import { Base64 } from 'js-base64';
+
 export function toGlobalId(type: string, id: string | number): string {
-	return Buffer.from(`${type}:${id}`).toString('base64');
+	return Base64.encode(`${type}:${id}`);
 }
 
 export function fromGlobalId(globalId: string): { type: string; id: string } {
-	const [type, id] = Buffer.from(globalId, 'base64').toString('utf8').split(':');
+	const [type, id] = Base64.decode(globalId).split(':');
 	return { type, id };
 }
