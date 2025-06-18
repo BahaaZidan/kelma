9879faef415a98153efd4168f50af6d5068ae3c2
BahaaZidan <gebnatorky@gmail.com>
2025-06-18 08:22:16 +0300

    use vite-plugin-watch-and-run instead of the hardcoded plugin to add vite watch to graphql codegen

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { kitRoutes } from 'vite-plugin-kit-routes';
-
-import { graphqlCodegen } from './vite-plugins/graphql-codegen';
+import { watchAndRun } from 'vite-plugin-watch-and-run';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
 		kitRoutes({ generated_file_path: 'src/lib/__generated__/routes.ts' }),
-		graphqlCodegen(),
+		watchAndRun([
+			{
+				name: 'graphql:gen',
+				watch: [path.resolve('src/lib/graphql/schema.graphql'), path.resolve('graphql.config.ts')],
+				run: 'pnpm graphql:gen',
+				delay: 10,
+				logs: ['streamError'],
+			},
+		]),
 	],
 });
