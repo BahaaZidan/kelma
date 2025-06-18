57555321389bc505d47495628c562624ad2aa338
BahaaZidan <gebnatorky@gmail.com>
2025-06-18 14:41:12 +0300

    remove tanstack query and its utils

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-
-	import { browser } from '$app/environment';
-
 	import { Toasts } from '$lib/client/toasts.svelte';
 
 	let { children } = $props();
-
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				enabled: browser,
-			},
-		},
-	});
 </script>
 
 <div class="toast toast-end">
@@ -31,6 +19,4 @@
 	{/each}
 </div>
 
-<QueryClientProvider client={queryClient}>
-	{@render children()}
-</QueryClientProvider>
+{@render children()}
