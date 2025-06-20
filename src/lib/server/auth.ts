ae4b5572bfc602c89b152a47d2a56cc9543954e1
BahaaZidan <gebnatorky@gmail.com>
2025-06-20 11:49:26 +0300

    comment github env vars

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+// import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 import { db } from './db';
 
@@ -14,8 +14,8 @@ export const auth = betterAuth({
 	},
 	socialProviders: {
 		github: {
-			clientId: GITHUB_CLIENT_ID,
-			clientSecret: GITHUB_CLIENT_SECRET,
+			clientId: 'Iv33lifPE8V2JMMVCsML',
+			clientSecret: 'feb01cf23455d22ef5d655ed5fd3030ce85bd86q',
 		},
 	},
 	// TODO: only trust paying customers
