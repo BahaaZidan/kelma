1e3a5fd3530307d8464577409ee9d2f9137f817d
BahaaZidan <gebnatorky@gmail.com>
2025-06-29 17:55:34 +0300

    fix houdini client url

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
+import { BETTER_AUTH_URL } from '$env/static/private';
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-	url: 'http://localhost:5173/api/graphql',
+	url: `${BETTER_AUTH_URL}/api/graphql`,
 
 	// uncomment this to configure the network call (for things like authentication)
 	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
