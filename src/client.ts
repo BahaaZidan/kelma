8484389d29acb8faa9831fb81aed6a5ed449f049
BahaaZidan <gebnatorky@gmail.com>
2025-07-12 11:08:45 +0300

    use relative url in houdini's client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,5 @@
-import { env } from '$env/dynamic/public';
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-	url: `${env.PUBLIC_BASE_URL}/api/graphql`,
-
-	// uncomment this to configure the network call (for things like authentication)
-	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-	// fetchParams({ session }) {
-	//     return {
-	//         headers: {
-	//             Authorization: `Bearer ${session.token}`,
-	//         }
-	//     }
-	// }
+	url: '/api/graphql',
 });
