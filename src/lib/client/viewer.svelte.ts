c4bc4686146b21fc9b05f602968294c2bbbf9825
BahaaZidan <gebnatorky@gmail.com>
2025-07-12 12:15:45 +0300

    make viewer context types easier to read

--- src/lib/client/viewer.svelte.ts
+++ src/lib/client/viewer.svelte.ts
@@ -2,14 +2,14 @@ import { getContext, setContext } from 'svelte';
 
 import type { QueryResult, ViewerQuery$result } from '$houdini';
 
-type Viewer = NonNullable<QueryResult<ViewerQuery$result, null>['data']>['viewer'] | undefined;
+type Viewer = NonNullable<QueryResult<ViewerQuery$result, null>['data']>['viewer'];
 
 const key = {};
 
-export function setViewerContext(viewerGetter: () => Viewer) {
+export function setViewerContext(viewerGetter: () => Viewer | undefined | null) {
 	setContext(key, viewerGetter());
 }
 
 export function getViewerContext() {
-	return getContext(key) as Viewer;
+	return getContext(key) as Viewer | undefined | null;
 }
