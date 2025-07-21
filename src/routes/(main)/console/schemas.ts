d15f8d7e40729e3aa149f36e8b023ca6a633369b
BahaaZidan <gebnatorky@gmail.com>
2025-07-21 12:14:33 +0300

    regex to accept domains

--- src/routes/(main)/console/schemas.ts
+++ src/routes/(main)/console/schemas.ts
@@ -2,6 +2,15 @@ import * as v from 'valibot';
 
 export const baseInfoSchema = v.object({
 	name: v.pipe(v.string(), v.trim(), v.minLength(2), v.maxLength(50)),
-	// TODO: decent domain validation
-	domains: v.pipe(v.array(v.pipe(v.string(), v.trim(), v.minLength(4))), v.minLength(1)),
+	domains: v.pipe(
+		v.array(
+			v.pipe(
+				v.string(),
+				v.trim(),
+				v.minLength(4),
+				v.regex(/^(localhost|(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$/)
+			)
+		),
+		v.minLength(1)
+	),
 });
