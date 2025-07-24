3d40a4b56d4717e1728867a11ec5dfc75ccb1be6
BahaaZidan <gebnatorky@gmail.com>
2025-07-24 19:00:02 +0300

    back to +layout.gql

--- src/routes/+layout.ts
+++
@@ -1,9 +0,0 @@
-import { load_ViewerQuery } from '$houdini';
-
-import type { LayoutLoad } from './$houdini';
-
-export const load: LayoutLoad = async (event) => {
-	return {
-		...(await load_ViewerQuery({ event })),
-	};
-};
