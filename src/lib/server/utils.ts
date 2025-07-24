6a9fc234469ead0ec19e3d869666aec1edbf1c1a
BahaaZidan <gebnatorky@gmail.com>
2025-07-24 11:01:18 +0300

    default for trusting domains for now

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,6 +1,8 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
 export function isDomainTrusted(request: RequestEvent['request'], trustedDomains: string[]) {
+	// TODO: this is probably wrong
+	if (request.referrer === 'about:client') return true;
 	const referer = request.headers.get('Referer');
 	if (!referer) return false;
 
