996ce7fea1006bf8a405d7b445b66818d3ffc972
BahaaZidan <gebnatorky@gmail.com>
2025-07-26 10:04:06 +0300

    better InputFieldError

--- src/lib/client/components/InputFieldError.svelte
+++ src/lib/client/components/InputFieldError.svelte
@@ -2,8 +2,10 @@
 	let { errors = [] }: { errors?: string[] } = $props();
 </script>
 
-{#each errors as error (error)}
-	<div class="text-error text-xs">
-		{error}
-	</div>
-{/each}
+<div class="mt-1 flex flex-col gap-0.5">
+	{#each errors as error (error)}
+		<div class="text-error text-xs">
+			{error}
+		</div>
+	{/each}
+</div>
