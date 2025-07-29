5973fb533768e70940253ca0ad85aad2d48ad07f
BahaaZidan <gebnatorky@gmail.com>
2025-07-29 16:06:28 +0300

    same

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,10 +1,12 @@
+import { redirect } from '@sveltejs/kit';
 import * as v from 'valibot';
 
 import type { PageServerLoad } from './$types';
 
 const schema = v.pipe(v.string(), v.trim(), v.url());
 
-export const load: PageServerLoad = async ({ url }) => {
+export const load: PageServerLoad = async ({ url, locals }) => {
+	if (locals.session?.user) throw redirect(303, '/console');
 	const callbackURL = v.parse(schema, url.searchParams.get('callback_url'));
 
 	return { url: url.toString(), callbackURL };
