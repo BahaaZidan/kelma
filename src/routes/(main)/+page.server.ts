a16a67ecbd06be4b9c37c5e31942fceb1433f11a
BahaaZidan <gebnatorky@gmail.com>
2025-07-29 16:10:39 +0300

    headache

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,7 +1,12 @@
-import { redirect } from '@sveltejs/kit';
+import { env } from '$env/dynamic/public';
+
+import { route } from '$lib/routes';
 
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	if (locals.session?.user) return redirect(303, '/console');
+	if (locals.session?.user) return { cta_route: route('/console') };
+	return {
+		cta_route: `${route('/login')}?callback_url=${env.PUBLIC_BASE_URL + route('/console')}`,
+	};
 };
--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -2,18 +2,16 @@
 	import MailIcon from '@lucide/svelte/icons/mail';
 	import { siGithub } from 'simple-icons';
 
-	import { env } from '$env/dynamic/public';
-
 	import BrandIcon from '$lib/client/components/BrandIcon.svelte';
 	import { PAGEVIEW_COST_IN_CENTS } from '$lib/constants';
 	import { LANGS } from '$lib/i18n';
-	import { route } from '$lib/routes';
 
+	import type { PageProps } from './$types';
 	import FeatureCard from './FeatureCard.svelte';
 
+	let { data }: PageProps = $props();
 	let price_slider_val = $state(100_000);
 
-	const login_url = `${route('/login')}?callback_url=${env.PUBLIC_BASE_URL + route('/console')}`;
 	const iconClass = 'transition-all duration-300 ease-in-out hover:text-gray-700';
 
 	const canonicalURL = 'https://kelma.dev/';
@@ -50,7 +48,7 @@
 			Ad-free, privacy-respecting, open-source, and insanely fast. Effortless setup with easy
 			customization and fair usage-based pricing.
 		</p>
-		<a href={login_url} class="btn btn-primary btn-lg">Try it for free</a>
+		<a href={data.cta_route} class="btn btn-primary btn-lg">Try it for free</a>
 	</section>
 
 	<section class="bg-base-200 rounded-box px-6 py-10">
@@ -103,7 +101,7 @@
 	<section id="get-started" class="px-6 py-6 text-center">
 		<h2 class="mb-4 text-3xl font-bold">Get 10,000 Free Page Views</h2>
 		<p class="mb-6">No credit card required. Start engaging your audience today.</p>
-		<a href={login_url} class="btn btn-accent btn-lg">Create Free Account</a>
+		<a href={data.cta_route} class="btn btn-accent btn-lg">Create Free Account</a>
 	</section>
 
 	<section class="bg-base-200 rounded-box flex flex-col items-center gap-2 p-6">
