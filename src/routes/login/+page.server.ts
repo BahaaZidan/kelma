8b23d47e96d1aaf9701bd15c9a27aa0931e5424f
BahaaZidan <gebnatorky@gmail.com>
2025-07-30 09:12:24 +0300

    safe behaviour instead of safe assumptions

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,13 +1,14 @@
-import { redirect } from '@sveltejs/kit';
 import * as v from 'valibot';
+
+import { env } from '$env/dynamic/public';
 
 import type { PageServerLoad } from './$types';
 
 const schema = v.pipe(v.string(), v.trim(), v.url());
 
-export const load: PageServerLoad = async ({ url, locals }) => {
-	if (locals.session?.user) throw redirect(303, '/console');
-	const callbackURL = v.parse(schema, url.searchParams.get('callback_url'));
+export const load: PageServerLoad = async ({ url }) => {
+	const validation = v.safeParse(schema, url.searchParams.get('callback_url'));
+	const callbackURL = validation.success ? validation.output : env.PUBLIC_BASE_URL;
 
 	return { url: url.toString(), callbackURL };
 };
